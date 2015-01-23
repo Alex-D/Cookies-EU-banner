4037554e6fe33d2938ea413d231f54538f9836cd
Alex-D <contact@alex-d.fr>
2015-01-23 15:26:18 +0100

    Add some functions

--- cookies-cnil-banner.js
+++ cookies-cnil-banner.js
@@ -2,6 +2,11 @@
 
 (function(navigator, window, document){
 	CookiesCnilBanner = function(launchFunction){
+		if(!(this instanceof CookiesCnilBanner)){
+	        return new CookiesCnilBanner(launchFunction);
+		}
+
+		this.cookieTimeout = 33696000000; // 13 months in milliseconds
 		this.bots = /bot|googlebot|crawler|spider|robot|crawling/i;
 		this.trackingCookiesNames = ["__utma","__utmb","__utmc","__utmt","__utmv","__utmz","_ga","_gat"];
 		this.launchFunction = launchFunction;
@@ -12,18 +17,34 @@
 		init: function(){
 			// Do nothing if it is a bot
 			// If DoNotTrack is activated, do nothing too
-			if(this.isBot() || !this.isToTrack()){
+			if(this.isBot() || !this.isToTrack() || this.hasConsent() === false){
 				return false;
 			}
 
-			this.launchFunction();
+			// User has already consent to use cookies to tracking
+			if(this.hasConsent() === true){
+				// Launch user custom function
+				this.launchFunction();
+				return true;
+			}
+
+			// If it's not a bot, no DoNotTrack and not already accept : show banner
+			this.showBanner();
+
+			// By default, accept cookies for the next if user do nothing
+			this.setCookie('hasConsent', true);
 		},
 
 		/*
 		 * Check if user already consent
 		 */
 		hasConsent: function(){
-			return document.cookie.indexOf("hasConsent=true") > -1;
+			if(document.cookie.indexOf("hasConsent=true") > -1){
+				return true;
+			} else if(document.cookie.indexOf("hasConsent=false") > -1){
+				return false;
+			}
+			return null;
 		},
 
 		/*
@@ -49,18 +70,23 @@
 	    deleteTrackingCookies: function(){
 	    	var name;
 	    	for(name in this.trackingCookiesNames){
-
+	    		this.deleteCookie(name);
 	    	}
+	    },
+
+	    setCookie: function(name, value){
+		    var date = new Date();
+		    date.setTime(date.getTime() + this.cookieTimeout);
+
+	    	document.cookie = name + "=" + hasconsent + "; expires=" + date.toGMTString() + "; path=/";
 	    },
 
 	    deleteCookie: function(name){
 	        var hostname = document.location.hostname;
 	        if(hostname.indexOf("www.") === 0){
 	            hostname = hostname.substring(4);
-	        }
-	        var domain = ";domain=" + "."+hostname;
-	        var expiration = "Thu, 01-Jan-1970 00:00:01 GMT";       
-	        document.cookie = name + "=;path=/" + domain + ";expires=Thu, 01-Jan-1970 00:00:01 GMT";
+	        }     
+	        document.cookie = name + "=;path=/;domain=." + hostname + ";expires=Thu, 01-Jan-1970 00:00:01 GMT";
 	    }
 	};
 })(navigator, window, document);

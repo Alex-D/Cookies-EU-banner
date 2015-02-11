40883db079f9b83fd5ebeedd10dd453cbafe857e
Alex-D <contact@alex-d.fr>
2015-02-11 17:10:19 +0100

    Update for UMD JS

--- cookies-eu-banner.js
+++ cookies-eu-banner.js
@@ -1,13 +1,13 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
+    if (typeof define === "function" && define.amd){
         define([], factory);
-    } else if (typeof exports === 'object') {
+    } else if (typeof exports === "object"){
         module.exports = factory();
     } else {
         // root is window
         root.CookiesEuBanner = factory();
-  }
-}(this, function () {
+    }
+}(this, function(){
 
     var CookiesEuBanner = function(launchFunction){
         if(!(this instanceof CookiesEuBanner)){
